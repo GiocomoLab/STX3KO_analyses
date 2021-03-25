9b48eaa164df229dcf49f1ffa58c3641cf353c19
markplitt <markplitt@gmail.com>
2021-03-25 01:08:47 -0700

    add morph session class

--- STX3KO_analyses/session.py
+++ STX3KO_analyses/session.py
@@ -165,17 +165,20 @@ class MorphSession(TwoPUtils.sess.Session):
 
         :return:
         """
-        self.trial_info = {'morph': self._get_morph()}
+
+        morph_shared, wall_jitter = self._get_morph()
+        self.trial_info = {'morph_shared': morph_shared, 'morph': morph_shared+wall_jitter}
 
     def _get_morph(self):
         """
 
         :return:
         """
-        morph_trial = np.zeros(self.teleport_inds.shape)
+        morph_trial, wall_jitter = np.zeros(self.teleport_inds.shape), np.zeros(self.teleport_inds.shape)
         for i, (start, stop) in enumerate(zip(self.trial_start_inds.tolist(), self.teleport_inds.tolist())):
-            morph_trial[i] = self.vr_data['LR'].iloc[start + 10]
-        return morph_trial
+            morph_trial[i] = self.vr_data['morph'].iloc[start + 10]
+            wall_jitter[i] = self.vr_data['wallJitter'].iloc[start + 10]
+        return morph_trial, wall_jitter
 
     def add_pos_binned_trial_matrix(self, ts_name, pos_key='pos', min_pos=0, max_pos=450, bin_size=10, mat_only=True,
                                     **trial_matrix_kwargs):
@@ -217,18 +220,16 @@ class MorphSession(TwoPUtils.sess.Session):
                 self.timeseries[Fneukey][cell:cell + 1, :].T)
 
         print('pre dff inds')
-        dff = sp.ndimage.median_filter(Freg[:, start_ind:stop_ind], size=(1, 7))
-        dff[:, :] = TwoPUtils.utilities.dff(dff[:, :], **dff_kwargs)
+        dff = sp.ndimage.median_filter(Freg, size=(1, 7))
+        dff= TwoPUtils.utilities.dff(dff, **dff_kwargs)
 
-        spks = dcnv.oasis(dff, 2000, self.s2p_ops['tau'],
-                                                 self.scan_info['frame_rate'])
+        spks = dcnv.oasis(dff, 2000, self.s2p_ops['tau'], self.scan_info['frame_rate'])
 
         self.add_timeseries(**{key_out: dff, 'spks': spks})
         self.add_pos_binned_trial_matrix(key_out)
         self.add_pos_binned_trial_matrix('spks')
 
-    def place_cells_calc(self, Fkey='F_dff', trial_mask=None, lr_split=True, out_key=None, min_pos=13, max_pos=43,
-                         bin_size=1, **pc_kwargs):
+    def place_cells_calc(self, Fkey='F_dff', morph_split = True, out_key=None, bin_size=10, **pc_kwargs):
 
         # choose appropriate target dictionary
         if out_key is None:
@@ -237,26 +238,23 @@ class MorphSession(TwoPUtils.sess.Session):
             self.place_cell_info.update({out_key: {}})
             d = self.place_cell_info[out_key]
 
-        if trial_mask is None:
-            trial_mask = np.ones(self.trial_start_inds.shape) > 0
 
-        if lr_split:
+        if morph_split:
 
-            lr_masks = {'left': (self.trial_info['LR'] == -1) * trial_mask,
-                        'right': (self.trial_info['LR'] == 1) * trial_mask}
-            for key, mask in lr_masks.items():
-                masks, SI, p = TwoPUtils.spatial_analyses.place_cells_calc(self.timeseries[Fkey].T, self.vr_data['t'],
+            morph_masks = {m: (self.trial_info['morph_shared'] == m) for m in [0, .25, .5, .75, 1]}
+
+            for key, mask in morph_masks.items():
+                masks, si, p = TwoPUtils.spatial_analyses.place_cells_calc(self.timeseries[Fkey].T, self.vr_data['t'],
                                                                            self.trial_start_inds[mask],
                                                                            self.teleport_inds[mask],
-                                                                           min_pos=min_pos, max_pos=max_pos,
                                                                            bin_size=bin_size, **pc_kwargs)
 
-                d[key] = {'masks': masks, 'SI': SI, 'p': p}
+                d[key] = {'masks': masks, 'SI': si, 'p': p}
         else:
-            mask = trial_mask
-            masks, SI, p = TwoPUtils.spatial_analyses.place_cells_calc(self.timeseries[Fkey].T, self.vr_data['t'],
-                                                                       self.trial_start_inds[mask],
-                                                                       self.teleport_inds[mask],
+
+            masks, si, p = TwoPUtils.spatial_analyses.place_cells_calc(self.timeseries[Fkey].T, self.vr_data['t'],
+                                                                       self.trial_start_inds,
+                                                                       self.teleport_inds,
                                                                        min_pos=min_pos, mas_pos=max_pos,
                                                                        bin_size=bin_size, **pc_kwargs)
-            d.update({'masks': masks, 'SI': SI, 'p': p})
+            d.update({'masks': masks, 'SI': si, 'p': p})
