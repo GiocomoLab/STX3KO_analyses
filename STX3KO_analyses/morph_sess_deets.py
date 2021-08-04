ed50557e4af34a36011e6535d3f4f8e49bada5a2
markplitt <markplitt@gmail.com>
2021-08-04 11:50:21 -0700

    add morph session details

--- STX3KO_analyses/morph_sess_deets.py
+++ STX3KO_analyses/morph_sess_deets.py
@@ -36,14 +36,37 @@ KO_sessions = {'4467975.1': ({'date': '09_10_2020', 'scene': 'TwoTower_foraging'
 
                }
 
-CT_sessions = {'4467331.1': ({'date': '09_10_2020', 'scene': 'TwoTower_foraging', 'session': 4, 'scan': 11},
-                             # {'date': '11_10_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 4},
-                             # {'date': '11_10_2020', 'scene': 'TwoTower_foraging', 'session': 4, 'scan': 10},
-                             # {'date': '13_10_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 5},
-                             # {'date': '14_10_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 3},
-                             # {'date': '15_10_2020', 'scene': 'TwoTower_foraging', 'session': 3, 'scan': 6},
-                             # {'date': '16_10_2020', 'scene': 'TwoTower_foraging', 'session': 3, 'scan': 7},
-                             # {'date': '17_10_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 2},
-                             # ),
+CT_sessions = {'4467331.1': ({'date': '10_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 1},
+                             {'date': '13_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 2},
+                             {'date': '15_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 2},
+                             {'date': '16_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 4},
+                             {'date': '18_12_2020', 'scene': 'TwoTower_foraging', 'session': 1, 'scan': 7},
+                             {'date': '19_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 2},
+                             ),
 
-               # '4467975.2': ({'date': '09_10_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 2},
+
+               '4467331.2': ({'date': '10_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 2},
+                             {'date': '14_12_2020', 'scene': 'TwoTower_foraging', 'session': 1, 'scan': 3},
+                             {'date': '15_12_2020', 'scene': 'TwoTower_foraging', 'session': 3, 'scan': 5},
+                             {'date': '16_12_2020', 'scene': 'TwoTower_foraging', 'session': 4, 'scan': 4},
+                             {'date': '17_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 5},
+                             {'date': '18_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 3},
+                             {'date': '19_12_2020', 'scene': 'TwoTower_foraging', 'session': 3, 'scan': 6},
+                             ),
+
+               '4467332.1': ({'date': '11_12_2020', 'scene': 'TwoTower_foraging', 'session': 3, 'scan': 4},
+                             {'date': '14_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 1},
+                             {'date': '16_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 2},
+                             {'date': '17_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 2},
+                             {'date': '18_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 2},
+                             {'date': '19_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 4},
+                             ),
+
+               '4467333.1': ({'date': '11_12_2020', 'scene': 'TwoTower_foraging', 'session': 3, 'scan': 4},
+                             {'date': '14_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 1},
+                             {'date': '16_12_2020', 'scene': 'TwoTower_foraging', 'session': 3, 'scan': 1},
+                             {'date': '17_12_2020', 'scene': 'TwoTower_foraging', 'session': 3, 'scan': 4},
+                             {'date': '18_12_2020', 'scene': 'TwoTower_foraging', 'session': 2, 'scan': 2},
+                             {'date': '19_12_2020', 'scene': 'TwoTower_foraging', 'session': 3, 'scan': 6},
+                             ),
+               }
