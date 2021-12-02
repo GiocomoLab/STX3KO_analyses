30c7ccd2fab6f1615a05dd7ddba2b5fb77956c91
markplitt <markplitt@gmail.com>
2021-12-02 00:01:00 -0800

    delete spks norm fast from concat session

--- STX3KO_analyses/utilities.py
+++ STX3KO_analyses/utilities.py
@@ -120,8 +120,8 @@ def load_single_day(mouse, day, verbose = True):
             sess_list.append(_sess)
 
         sess = session.ConcatYMazeSession(sess_list, common_roi_mapping, day_inds=[0 for i in range(len(deets))],
-                                          trial_mat_keys=('F_dff', 'spks', 'F_dff_norm', 'spks_norm','spks_norm_fast','licks', 'speed'),
-                                          timeseries_keys=('F_dff', 'spks', 'F_dff_norm', 'spks_norm','spks_norm_fast','licks', 'speed'),
+                                          trial_mat_keys=('F_dff', 'spks', 'F_dff_norm', 'spks_norm','licks', 'speed'),
+                                          timeseries_keys=('F_dff', 'spks', 'F_dff_norm', 'spks_norm','licks', 'speed'),
                                           run_place_cells=True)
         if mouse in ['4467332.2'] and day == 0:
             mask = sess.trial_info['sess_num_ravel'] > 0
