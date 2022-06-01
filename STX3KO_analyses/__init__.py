0681f0623d0cfbe1b076bd76b656a66aba1b90d1
markplitt <markplitt@gmail.com>
2022-06-01 02:49:17 -0700

    massive speed up plotting of roi_check using "set_data"

--- STX3KO_analyses/__init__.py
+++ STX3KO_analyses/__init__.py
@@ -1,1 +1,2 @@
-from . import session, ymaze_sess_deets, morph_sess_deets, behavior, wta, spatial_analyses, reward_overrep, single_cell_stats
+from . import session, ymaze_sess_deets, morph_sess_deets, behavior, wta, spatial_analyses, reward_overrep
+from . import single_cell_stats, roi_check
