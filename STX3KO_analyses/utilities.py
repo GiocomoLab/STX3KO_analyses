eac5beea738d82ad0a0bf81e9537be128dd005c7
markplitt <markplitt@gmail.com>
2023-05-25 00:46:45 -0700

    add spks_nostop

--- STX3KO_analyses/utilities.py
+++ STX3KO_analyses/utilities.py
@@ -140,7 +140,7 @@ def load_single_day(mouse, day, verbose = True, pkl_basedir = '/home/mplitt/YMaz
         sess = session.ConcatYMazeSession(sess_list, common_roi_mapping, day_inds=[0 for i in range(len(deets))],
                                           trial_mat_keys=('F_dff', 'spks', 'spks_th', 'F_dff_norm', 'spks_norm','licks', 'speed'),
                                           timeseries_keys=('F_dff', 'spks', 'spks_th', 'F_dff_norm', 'spks_norm','licks', 'speed', 
-                                                           't', 'LR', 'reward', 'block_number'),
+                                                           't', 'LR', 'reward', 'block_number', 'spks_nostop'),
                                           run_place_cells=True)
         if mouse in ['4467332.2'] and day == 0:
             mask = sess.trial_info['sess_num_ravel'] > 0
@@ -274,6 +274,6 @@ def single_mouse_concat_sessions(mouse, date_inds=None, load_ops = False, load_s
     concat_sess = session.ConcatYMazeSession(sess_list, common_roi_mapping, day_inds=date_inds_ravel,
                                              trial_mat_keys=['F_dff', 'F_dff_norm', 'spks', 'spks_th', 'spks_norm', 'licks', 'speed'],
                                              timeseries_keys=('F_dff', 'spks', 'spks_th', 'F_dff_norm', 'spks_norm','licks', 'speed', 
-                                                           't', 'LR', 'reward', 'block_number'),
+                                                           't', 'LR', 'reward', 'block_number', 'spks_no_stop'),
                                              load_ops=load_ops, load_stats = load_stats)
     return concat_sess
