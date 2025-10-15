3fa2ca18fbe7426d72ea5b45d751cafbc4a04aa7
markplitt <markplitt@gmail.com>
2025-10-15 12:58:49 -0700

    figure making

--- STX3KO_analyses/naive_bayes.py
+++ STX3KO_analyses/naive_bayes.py
@@ -1,17 +1,18 @@
+from STX3KO_analyses import utilities as u
 
 import numpy as np
 import scipy as sp
 
 
-gamma = lambda x,kappa:  1/sp.special.gamma(kappa)* np.exp(-x)*np.power(x,kappa-1)
-poiss = lambda k,lam:  1/sp.special.gamma(k)* np.exp(-lam)*np.power(lam,k)
+gamma = lambda x,kappa:  1/(sp.special.gamma(kappa)+1E-5)* np.exp(-x)*np.power(x,kappa-1)
+poiss = lambda k,lam:  1/(sp.special.gamma(k)+1E-5)* np.exp(-lam)*np.power(lam,k)
 
 def logsumexp(x,axis=-1):
     c = x.max(axis = axis, keepdims=True) + 1E-5
     return c + np.log(np.sum(np.exp(x - c ),axis=axis, keepdims=True))
 
 
-def crossval(sess, n_cells = -1, poisson = False, nov = False, chan=None):
+def crossval(sess, n_cells = -1, poisson = True, nov = False, chan=None):
     
     # assuming uniform prior over bins
     if nov:
@@ -23,7 +24,9 @@ def crossval(sess, n_cells = -1, poisson = False, nov = False, chan=None):
         trial_mat = sess.trial_matrices['F_dff'][trial_mask,:,:]*10
     else:
         trial_mat = sess.trial_matrices[f'{chan}_F_dff'][trial_mask,:,:]*10
-    trial_mat -= np.amin(trial_mat,axis=-1,keepdims=True)
+    
+    trial_mat -= np.nanmin(trial_mat,axis=-1,keepdims=True)
+    trial_mat[np.isnan(trial_mat)]=0
     trial_mat += 1E-3
 
     # select cells
@@ -46,7 +49,7 @@ def crossval(sess, n_cells = -1, poisson = False, nov = False, chan=None):
         y[np.isnan(y)]=1E-3
         log_likelihood = 0
         for cell in range(trial_mat.shape[-1]):
-            if poiss:
+            if poisson:
                 log_likelihood += np.log(poiss(y[:,cell:cell+1], trial_mat_mean[:,cell:cell+1].T)+1E-3)
             else:
                 log_likelihood += np.log(gamma(y[:,cell:cell+1], trial_mat_mean[:,cell:cell+1].T)+1E-3)
@@ -105,11 +108,35 @@ def run_alldays_baseline_crossval(mice, n_cells):
             
             fam_post = []
             nov_post = []
-            for rep in range(50):
+            for rep in range(10):
                 fam_post.append(crossval(sess,n_cells=n_cells, nov=False))
                 nov_post.append(crossval(sess, n_cells=n_cells, nov=True))
             fam_post = np.concatenate(fam_post, axis=0)
             nov_post = np.concatenate(nov_post, axis=0)
             
             results[mouse][day] = {'fam': fam_post, 'nov': nov_post}
+    return results
+
+def run_alldays_baseline_crossval_sparse(mice, n_cells):
+    results = {}
+    for mouse in mice:
+        print('mouse', mouse)
+        results[mouse]={}
+        for day in range(6):
+            if mouse == 'SparseKO_09' and day ==2:
+                continue
+            results[mouse][day]={}
+            sess = u.load_single_day(mouse,day=day)
+            
+            for chan in ('channel_0', 'channel_1'):
+                results[mouse][day][chan]={}
+                fam_post = []
+                nov_post = []
+                for rep in range(10):
+                    fam_post.append(crossval(sess,n_cells=n_cells, nov=False, chan=chan))
+                    nov_post.append(crossval(sess, n_cells=n_cells, nov=True, chan=chan))
+                fam_post = np.concatenate(fam_post, axis=0)
+                nov_post = np.concatenate(nov_post, axis=0)
+            
+                results[mouse][day][chan] = {'fam': fam_post, 'nov': nov_post}
     return results
