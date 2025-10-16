cc963100f21bee5a206486139cc6c32779be0276
markplitt <markplitt@gmail.com>
2025-10-16 12:58:41 -0700

    finish dense code

--- notebooks/figure3/run_place_field_shuffle_models.py
+++ notebooks/figure3/run_place_field_shuffle_models.py
@@ -96,7 +96,11 @@ def run_n_fields_dense():
         model_result = model.fit()
 
         # run genotype shuffles
-        shuff_coefs = np.array(joblib.Parallel(n_jobs=16)(joblib.delayed(run_model)(df.copy(), ctrl_mice+ko_mice, comb, 'rank_n_fields') for i, comb in enumerate(itertools.combinations(ctrl_mice+ko_mice,len(ctrl_mice))) if i <10))
+        shuff_coefs = np.array(joblib.Parallel(n_jobs=16)(joblib.delayed(run_model)(df.copy(), 
+                                                                                    ctrl_mice+ko_mice, 
+                                                                                    comb, 
+                                                                                    stat_column,
+                                ) for i, comb in enumerate(itertools.combinations(ctrl_mice+ko_mice,len(ctrl_mice))) ))
 
 
 
@@ -104,4 +108,78 @@ def run_n_fields_dense():
                                     'genotype coef.': model_result.fe_params['cre[T.ko]'],
                                     'shuffle genotype coef.': shuff_coefs,
                                     }
+        
+    return result_dict
 
+
+
+def run_width_dense():
+
+
+    with open('/home/mplitt/shuffle_pkls/dense_place_field_spatial_shuffle_F_dff.pkl','rb') as file:
+        shuff_results = pickle.load(file)
+
+    df = {'cre': [], 'mouse': [], 'day': [], 'nov': [], 'width': []}
+    for key, mice in zip(('ctrl', 'ko'),(ctrl_mice, ko_mice)):
+        for mouse in mice:
+            for day in range(6):
+                for nov in ( 'fam', 'nov'):
+
+                    rising_edges = shuff_results[mouse][day][nov]['rising_edges']
+                    falling_edges = shuff_results[mouse][day][nov]['falling_edges']
+                    widths = shuff_results[mouse][day][nov]['field_widths']
+                
+                    mask = (widths>1) * (widths<25) * (rising_edges[:,1]>0) * (falling_edges[:,1]<29)
+                    widths = widths[mask]
+                    
+
+                    for w in widths:
+                        df['cre'].append(key)
+                        df['mouse'].append(mouse)
+                        df['day'].append(day)
+                        df['nov'].append(nov)
+                
+                        df['width'].append(w*10)
+
+    df = pd.DataFrame.from_dict(df)
+
+
+    df['cre'] = df["cre"].astype("category")
+    df["mouse"] = df["mouse"].astype("category")
+    df["day"] = df["day"].astype("category")
+    df["nov"] = df["nov"].astype("category")
+    df["rank_width"] = df["width"].rank()
+
+    result_dict = {}
+    for stat_column in ('width', 'rank_width'):
+        
+
+        # Fit a mixed model on ranks
+        model = smf.mixedlm(f"{stat_column} ~ cre * day * nov", df, groups=df["mouse"])
+        model_result = model.fit()
+
+        # run genotype shuffles
+        shuff_coefs = np.array(joblib.Parallel(n_jobs=16)(joblib.delayed(run_model)(df.copy(), 
+                                                                                    ctrl_mice+ko_mice, 
+                                                                                    comb, 
+                                                                                    stat_column,
+                                ) for i, comb in enumerate(itertools.combinations(ctrl_mice+ko_mice,len(ctrl_mice))) ))
+
+
+
+        result_dict[stat_column] = {'true_model': model_result,
+                                    'genotype coef.': model_result.fe_params['cre[T.ko]'],
+                                    'shuffle genotype coef.': shuff_coefs,
+                                    }
+        
+    return result_dict
+
+if __name__ == "__main__":
+
+    result_dict = run_n_fields_dense()
+    with open('/home/mplitt/shuffle_pkls/dense_n_fields_mixedlm_shuffle.pkl', 'wb') as file:
+        pickle.dump(result_dict, file)
+
+    result_dict = run_width_dense()
+    with open('/home/mplitt/shuffle_pkls/dense_width_mixedlm_shuffle.pkl', 'wb') as file:
+        pickle.dump(result_dict, file)
