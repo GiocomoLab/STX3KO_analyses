584e6a1f5d04cf07ba82b06f1e0fe847abc18b11
markplitt <markplitt@gmail.com>
2025-10-20 16:32:14 -0700

    work on quantification for novel rate

--- STX3KO_analyses/novel_activity_rate.py
+++ STX3KO_analyses/novel_activity_rate.py
@@ -262,6 +262,9 @@ class BlockTransitionActivityRate_Sparse:
                 self.activity_rates[mouse][day] = {}
                 
                 sess = u.load_single_day(mouse, day,verbose=False)
+                
+                speed_mat = sess.trial_matrices['speed']
+                lick_mat = sess.trial_matrices['licks']
 
 
                 block_start_trial_num = np.argwhere(sess.trial_info['block_number']==self.block)[0][0]
@@ -273,29 +276,36 @@ class BlockTransitionActivityRate_Sparse:
                 block_fam_mask = block_mask * (sess.trial_info['LR']==-1*sess.novel_arm)
                 block_nov_mask = block_mask * (sess.trial_info['LR']==sess.novel_arm)
 
-
+                
                 for chan in ('channel_0', 'channel_1'):
                     d = {}
 
                     _ts_key = f'{chan}_{self.ts_key}'
-
-
-
+                    trial_mat = sess.trial_matrices[_ts_key]
                 
                     # average over trials and positions
                     d['baseline_avg_act'] = np.nanmean(np.nanmean(sess.trial_matrices[_ts_key][baseline_trials,:,:],
                                                         axis=0, keepdims=True), axis=1, keepdims=True)
                     
-                    d['baseline_act'] = sess.trial_matrices[_ts_key][baseline_trials,:, :]
-                    d['block_act'] = sess.trial_matrices[_ts_key][block_mask, :, :]
-                    d['block_fam_act'] = sess.trial_matrices[_ts_key][block_fam_mask, :, :]
-                    d['block_nov_act'] = sess.trial_matrices[_ts_key][block_nov_mask, :, :]
+                    d['baseline_act'] = trial_mat[baseline_trials,:, :]
+                    d['block_act'] = trial_mat[block_mask, :, :]
+                    d['block_fam_act'] = trial_mat[block_fam_mask, :, :]
+                    d['block_nov_act'] = trial_mat[block_nov_mask, :, :]
 
+                    d['baseline_speed'] = speed_mat[baseline_trials, :]
+                    d['block_speed'] = speed_mat[block_mask, :]
+                    d['block_fam_speed'] = speed_mat[block_fam_mask, :]
+                    d['block_nov_speed'] = speed_mat[block_nov_mask, :]
+
+                    d['baseline_licks'] = lick_mat[baseline_trials, :]
+                    d['block_licks'] = lick_mat[block_mask, :]
+                    d['block_fam_licks'] = lick_mat[block_fam_mask, :]
+                    d['block_nov_licks'] = lick_mat[block_nov_mask, :]
 
 
                     self.activity_rates[mouse][day][chan] = d
 
-    def build_dataframe(self, norm = 'population', max_trial = 5):
+    def build_dataframe(self, norm = 'population', max_trial = 5, norm_behavior=True):
 
         rows = []
         
@@ -312,24 +322,86 @@ class BlockTransitionActivityRate_Sparse:
                         fam = np.nanmean(np.nanmean(data['block_fam_act'],axis=1),axis=-1)[:max_trial]/base.mean()
                         nov = np.nanmean(np.nanmean(data['block_nov_act'],axis=1), axis=-1)[:max_trial]/base.mean()
 
+                        base_speed = np.nanmean(data['baseline_speed'],axis=1)
+                        fam_speed = np.nanmean(data['block_fam_speed'],axis=1)[:max_trial]
+                        nov_speed = np.nanmean(data['block_nov_speed'], axis=-1)[:max_trial]
+
+                        base_licks = np.nanmean(data['baseline_licks'],axis=1)
+                        fam_licks = np.nanmean(data['block_fam_licks'],axis=1)[:max_trial]
+                        nov_licks = np.nanmean(data['block_nov_licks'], axis=-1)[:max_trial]
+
+                        if norm_behavior:
+                            fam_speed = fam_speed/base_speed.mean()
+                            nov_speed = nov_speed/base_speed.mean()
+
+                            fam_licks = fam_licks/base_licks.mean()
+                            nov_licks = nov_licks/base_licks.mean()
+
                     elif norm == 'cell':
                         cell_denom = data['baseline_avg_act']
                         base = np.nanmean(np.nanmean(data['baseline_act']/cell_denom,axis=1),axis=-1)
                         fam = np.nanmean(np.nanmean(data['block_fam_act']/cell_denom,axis=1),axis=-1)[:max_trial]
                         nov = np.nanmean(np.nanmean(data['block_nov_act']/cell_denom,axis=1), axis=-1)[:max_trial]
+
+                        base_speed = np.nanmean(data['baseline_speed'],axis=1)
+                        fam_speed = np.nanmean(data['block_fam_speed'],axis=1)[:max_trial]
+                        nov_speed = np.nanmean(data['block_nov_speed'], axis=-1)[:max_trial]
+
+                        base_licks = np.nanmean(data['baseline_licks'],axis=1)
+                        fam_licks = np.nanmean(data['block_fam_licks'],axis=1)[:max_trial]
+                        nov_licks = np.nanmean(data['block_nov_licks'], axis=-1)[:max_trial]
+
+                        if norm_behavior:
+                            fam_speed = fam_speed/base_speed.mean()
+                            nov_speed = nov_speed/base_speed.mean()
+
+                            fam_licks = fam_licks/base_licks.mean()
+                            nov_licks = nov_licks/base_licks.mean()
                     elif norm is None:
                         base = np.nanmean(np.nanmean(data['baseline_act'],axis=1),axis=-1)
                         fam = np.nanmean(np.nanmean(data['block_fam_act'],axis=1),axis=-1)[:max_trial]
                         nov = np.nanmean(np.nanmean(data['block_nov_act'],axis=1), axis=-1)[:max_trial]
+
+                        base_speed = np.nanmean(data['baseline_speed'],axis=1)
+                        fam_speed = np.nanmean(data['block_fam_speed'],axis=1)[:max_trial]
+                        nov_speed = np.nanmean(data['block_nov_speed'], axis=-1)[:max_trial]
+
+                        base_licks = np.nanmean(data['baseline_licks'],axis=1)
+                        fam_licks = np.nanmean(data['block_fam_licks'],axis=1)[:max_trial]
+                        nov_licks = np.nanmean(data['block_nov_licks'], axis=-1)[:max_trial]
+
                     else:
                         raise ValueError("norm must be one of 'population', 'cell', or None")
                     
-                    rows.append({'channel': chan,
-                                'mouse': mouse,
-                                'day': day,
-                                'baseline_rate': base.mean(),
-                                'familiar_rate': fam.mean(),
-                                'novel_rate': nov.mean()})
+                    act = {'baseline': base.mean(),
+                           'familiar': fam.mean(),
+                           'novel': nov.mean()}
+                    speed = {'baseline': base_speed.mean(),
+                             'familiar': fam_speed.mean(),
+                             'novel': nov_speed.mean()}
+                    licks = {'baseline': base_licks.mean(),
+                             'familiar': fam_licks.mean(),
+                             'novel': nov_licks.mean()}
+                    for ttype in ('baseline', 'familiar', 'novel'):
+                        
+                        rows.append({'channel': chan,
+                                    'mouse': mouse,
+                                    'day': day,
+                                    'ttype': ttype,
+                                    'rate': act[ttype],
+                                    'speed': speed[ttype],
+                                    'licks': licks[ttype]})
+
+
+                                    # 'baseline_rate': base.mean(),
+                                    # 'familiar_rate': fam.mean(),
+                                    # 'novel_rate': nov.mean(),
+                                    # 'baseline_speed': base_speed.mean(),
+                                    # 'familiar_speed': fam_speed.mean(),
+                                    # 'novel_speed': nov_speed.mean(),
+                                    # 'baseline_licks': base_licks.mean(),
+                                    # 'familiar_licks': fam_licks.mean(),
+                                    # 'novel_licks': nov_licks.mean()})
     
         self.df = pd.DataFrame(rows)
         return self.df
